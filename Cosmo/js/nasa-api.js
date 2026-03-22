9b6d97cbd17810328ec07f5bba04c3cc0a6382b6
TIMZA11 <158054720+TIMZA11@users.noreply.github.com>
2026-03-22 17:57:55 +0700

    Update nasa-api.js

--- Cosmo/js/nasa-api.js
+++ Cosmo/js/nasa-api.js
@@ -11,81 +11,148 @@ const NasaAPI = (() => {
     const CACHE_TTL = 24 * 60 * 60 * 1000; // 24 часа
 
     // ─── Маппинг объектов каталога на поисковые запросы NASA ───
-    // Некоторые объекты лучше искать по англоязычным названиям
-
     const NASA_SEARCH_TERMS = {
         // Звёзды
-        sun: 'Sun star',
-        sirius: 'Sirius star',
-        betelgeuse: 'Betelgeuse',
-        polaris: 'Polaris star',
-        proxima: 'Proxima Centauri',
-        vega: 'Vega star',
-        altair: 'Altair star',
+        sun:          'Sun star',
+        sirius:       'Sirius star',
+        betelgeuse:   'Betelgeuse red supergiant',
+        polaris:      'Polaris star',
+        proxima:      'Proxima Centauri',
+        vega:         'Vega star',
+        altair:       'Altair star',
+        rigel:        'Rigel star blue supergiant',
+        antares:      'Antares red supergiant star',
+        aldebaran:    'Aldebaran red giant star',
+
         // Планеты
-        mercury: 'Mercury planet',
-        venus: 'Venus planet',
-        earth: 'Earth from space',
-        mars: 'Mars planet',
-        jupiter: 'Jupiter planet',
-        saturn: 'Saturn planet rings',
-        uranus: 'Uranus planet',
-        neptune: 'Neptune planet',
+        mercury:      'Mercury planet',
+        venus:        'Venus planet',
+        earth:        'Earth from space',
+        mars:         'Mars planet',
+        jupiter:      'Jupiter planet',
+        saturn:       'Saturn planet rings',
+        uranus:       'Uranus planet',
+        neptune:      'Neptune planet',
+
         // Карликовые планеты
-        pluto: 'Pluto New Horizons',
-        ceres: 'Ceres dwarf planet Dawn',
-        eris: 'Eris dwarf planet',
-        haumea: 'Haumea',
-        makemake: 'Makemake',
+        pluto:        'Pluto New Horizons',
+        ceres:        'Ceres dwarf planet Dawn',
+        eris:         'Eris dwarf planet',
+        haumea:       'Haumea dwarf planet',
+        makemake:     'Makemake dwarf planet',
+        gonggong:     'trans-neptunian object dwarf planet',
+        quaoar:       'Quaoar Kuiper Belt object',
+        sedna:        'Sedna distant dwarf planet',
+        orcus:        'Orcus trans-neptunian object',
+        salacia:      'Salacia Kuiper Belt',
+
         // Спутники
-        moon: 'Moon lunar',
-        io: 'Io Jupiter moon',
-        europa: 'Europa Jupiter moon',
-        ganymede: 'Ganymede moon',
-        titan: 'Titan Saturn moon',
-        enceladus: 'Enceladus Saturn',
-        triton: 'Triton Neptune moon',
+        moon:         'Moon lunar surface',
+        io:           'Io Jupiter moon volcanic',
+        europa:       'Europa Jupiter moon ocean',
+        ganymede:     'Ganymede moon Jupiter',
+        titan:        'Titan Saturn moon',
+        enceladus:    'Enceladus Saturn geysers',
+        triton:       'Triton Neptune moon',
+        callisto:     'Callisto Jupiter moon',
+        miranda:      'Miranda Uranus moon',
+        oberon:       'Oberon Uranus moon',
+
         // Астероиды
-        vesta: 'Vesta asteroid Dawn',
-        pallas: 'Pallas asteroid',
-        hygiea: 'Hygiea asteroid',
-        bennu: 'Bennu asteroid OSIRIS-REx',
-        itokawa: 'Itokawa asteroid',
+        vesta:        'Vesta asteroid Dawn',
+        pallas:       'Pallas asteroid',
+        hygiea:       'Hygiea asteroid',
+        bennu:        'Bennu asteroid OSIRIS-REx',
+        itokawa:      'Itokawa asteroid Hayabusa',
+        ryugu:        'Ryugu asteroid Hayabusa2',
+        apophis:      'Apophis asteroid near Earth',
+        eros:         'Eros asteroid NEAR',
+        ida:          'Ida asteroid Galileo',
+        mathilde:     'Mathilde asteroid NEAR',
+
         // Кометы
-        halley: 'Halley comet',
-        churyumov: 'comet 67P Churyumov',
-        'hale-bopp': 'Hale-Bopp comet',
-        neowise: 'NEOWISE comet',
+        halley:       'Halley comet',
+        churyumov:    'comet 67P Churyumov Rosetta',
+        'hale-bopp':  'Hale-Bopp comet',
+        neowise:      'NEOWISE comet',
+        encke:        'Encke comet',
+        wild2:        'Wild 2 comet Stardust',
+        tempel1:      'Tempel 1 comet Deep Impact',
+        hyakutake:    'Hyakutake comet',
+        shoemaker:    'Shoemaker-Levy 9 comet Jupiter impact',
+        lovejoy:      'Lovejoy comet',
+
         // Галактики
-        'milky-way': 'Milky Way galaxy',
-        andromeda: 'Andromeda galaxy M31',
-        triangulum: 'Triangulum galaxy M33',
-        lmc: 'Large Magellanic Cloud',
+        'milky-way':   'Milky Way galaxy center',
+        andromeda:     'Andromeda galaxy M31',
+        triangulum:    'Triangulum galaxy M33',
+        lmc:           'Large Magellanic Cloud',
+        smc:           'Small Magellanic Cloud',
+        whirlpool:     'Whirlpool galaxy M51',
+        sombrero:      'Sombrero galaxy M104',
+        cartwheel:     'Cartwheel galaxy',
+        'centaurus-a': 'Centaurus A galaxy NGC 5128',
+        bodes:         'Bode galaxy M81',
+
         // Туманности
-        'orion-nebula': 'Orion Nebula M42',
-        'crab-nebula': 'Crab Nebula M1',
-        'ring-nebula': 'Ring Nebula M57',
-        'eagle-nebula': 'Eagle Nebula pillars creation',
+        'orion-nebula':  'Orion Nebula M42',
+        'crab-nebula':   'Crab Nebula M1 supernova',
+        'ring-nebula':   'Ring Nebula M57',
+        'eagle-nebula':  'Eagle Nebula pillars of creation',
+        'helix-nebula':  'Helix Nebula eye of god',
+        'lagoon-nebula': 'Lagoon Nebula M8',
+        horsehead:       'Horsehead Nebula Orion',
+        butterfly:       'Butterfly Nebula NGC 6302',
+        'cat-eye':       'Cat Eye Nebula NGC 6543',
+        rosette:         'Rosette Nebula NGC 2244',
+
         // Чёрные дыры
-        'sgr-a': 'Sagittarius A black hole',
-        m87: 'M87 black hole',
-        'cygnus-x1': 'Cygnus X-1',
+        'sgr-a':       'Sagittarius A black hole milky way',
+        m87:           'M87 black hole Event Horizon Telescope',
+        'cygnus-x1':   'Cygnus X-1 black hole',
+        grs1915:       'GRS 1915 black hole X-ray binary',
+        'v404-cygni':  'V404 Cygni black hole X-ray',
+        'xte-j1650':   'black hole X-ray binary stellar',
+        ngc1277:       'NGC 1277 galaxy massive black hole',
+        holm15a:       'galaxy cluster Abell supermassive black hole',
+        'ic10-x1':     'IC 10 irregular galaxy black hole',
+        ss433:         'SS 433 microquasar jets',
+
         // Квазары
-        '3c273': 'quasar 3C 273',
-        apm08279: 'APM 08279 quasar gravitational lens',
-        ton618: 'TON 618 quasar black hole',
-        markarian: 'Markarian 231 galaxy quasar',
+        '3c273':      'quasar 3C 273 Hubble',
+        apm08279:     'APM 08279 quasar gravitational lens',
+        ton618:       'TON 618 quasar black hole',
+        markarian:    'Markarian 231 galaxy quasar',
+        '3c48':       'quasar 3C 48 radio source',
+        pks2349:      'quasar PKS distant galaxy',
+        hs1700:       'quasar distant universe Hubble',
+        b1422:        'quasar gravitational lens',
+        'sdss-j1030': 'high redshift quasar distant universe',
+        'ul-j0313':   'most distant quasar early universe',
+
         // Пульсары
-        'psr-b1919': 'pulsar neutron star radio',
+        'psr-b1919':   'pulsar neutron star radio waves',
         'crab-pulsar': 'Crab Nebula pulsar neutron star',
-        'psr-j0437': 'millisecond pulsar neutron star',
+        'psr-j0437':   'millisecond pulsar neutron star',
         'vela-pulsar': 'Vela supernova remnant pulsar',
+        millisecond:   'PSR B1257 millisecond pulsar planets',
+        'psr-j0030':   'pulsar neutron star X-ray',
+        'double-psr':  'double pulsar binary neutron star',
+        'psr-b1509':   'PSR B1509 nebula hand of god',
+        geminga:       'Geminga pulsar neutron star',
+        magnetar:      'SGR 1806 magnetar neutron star',
+
         // Экзопланеты
-        'proxima-b': 'Proxima Centauri b exoplanet',
-        'trappist-1e': 'TRAPPIST-1 exoplanet',
-        'kepler-452b': 'Kepler 452b',
-        '51-peg-b': 'exoplanet 51 Pegasi',
-        hd209458b: 'HD 209458 b exoplanet'
+        'proxima-b':    'Proxima Centauri b exoplanet',
+        'trappist-1e':  'TRAPPIST-1 exoplanet system',
+        'kepler-452b':  'Kepler 452b Earth-like exoplanet',
+        '51-peg-b':     'exoplanet 51 Pegasi hot Jupiter',
+        hd209458b:      'HD 209458 b exoplanet atmosphere',
+        'gliese-667cc': 'Gliese 667 exoplanet habitable zone',
+        'kepler-186f':  'Kepler 186f Earth-size exoplanet',
+        'trappist-1d':  'TRAPPIST-1 system habitable exoplanet',
+        'k2-18b':       'K2-18b super-Earth exoplanet ocean',
+        'toi-700d':     'TOI 700 d habitable zone exoplanet'
     };
 
     // ─── Кэширование ───
@@ -188,7 +255,6 @@ const NasaAPI = (() => {
             const json = await res.json();
 
             const items = json.collection?.items || [];
-            // Ищем ~medium или ~large версию (не orig — слишком большие)
             const medium = items.find(i => i.href?.includes('~medium')) ||
                            items.find(i => i.href?.includes('~large')) ||
                            items.find(i => i.href?.includes('~orig')) ||
